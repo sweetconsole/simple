fa2ff91e91d966eeb7456bd23c805f1ef8a052e3
sweetconsole <sweetconsole@gmail.com>
2025-09-11 19:56:25 +0300

    Исправление отображения на мобильных устройствах партнеров

--- src/components/widgets/Partners/Partners.tsx
+++ src/components/widgets/Partners/Partners.tsx
@@ -3,8 +3,11 @@ import { motion } from "framer-motion"
 import { Container } from "../../ui"
 import { partners } from "./partners.data.ts"
 import styles from "./Partners.module.scss"
+import { useMediaQuery } from "../../../hooks/useMediaQuery.ts"
 
 const Partners: FC = () => {
+	const isMobile = useMediaQuery("(max-width: 568px)")
+
 	return (
 		<section className={styles.block} id="partners">
 			<Container customStyles={styles.partners}>
@@ -14,8 +17,8 @@ const Partners: FC = () => {
 						href={`https://${partner.link}`}
 						target="_blank"
 						key={index}
-						initial={partner.initial}
-						whileInView={partner.whileInView}
+						initial={isMobile ? undefined : { ...partner.initial }}
+						whileInView={isMobile ? undefined : { ...partner.whileInView }}
 						transition={{ delay: 0.4, duration: 0.4 }}
 						viewport={{ once: true }}
 					>
